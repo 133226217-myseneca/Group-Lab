f83fe44da21daacbbc7b19d7f355374bb0612399
133226217-myseneca <sislam101@myseneca.ca>
2023-03-26 18:02:46 -0400

    Added gamerps-r1 second revise with PlayAgain feature

--- gamerps-i1.py
+++ gamerps-i1.py
@@ -12,26 +12,34 @@ The program was last updated on March 26, 2023.
 '''
 
 options = ["rock", "paper", "scissors"]
-computerOption = random.choice(options)
 
-print("Welcome to Rock, Paper, Scissors!")
-print("Please choose your option: rock, paper, or scissors")
-userOption = input().lower()
+while True:
+    computerOption = random.choice(options)
 
-if userOption not in options:
-    print("Invalid option. Please try again.")
-else:
-    print("You chose", userOption)
-    print("The computer chose", computerOption)
+    print("Welcome to Rock, Paper, Scissors!")
+    print("Please choose your option: rock, paper, or scissors")
+    userOption = input().lower()
 
-    if userOption == computerOption:
-        print("It's a tie!")
-    elif userOption == "rock" and computerOption == "scissors":
-        print("You win!")
-    elif userOption == "paper" and computerOption == "rock":
-        print("You win!")
-    elif userOption == "scissors" and computerOption == "paper":
-        print("You win!")
+    if userOption not in options:
+        print("Invalid option. Please try again.")
     else:
-        print("The computer wins!")
+        print("You chose", userOption)
+        print("The computer chose", computerOption)
 
+        if userOption == computerOption:
+            print("It's a tie!")
+        elif userOption == "rock" and computerOption == "scissors":
+            print("You win!")
+        elif userOption == "paper" and computerOption == "rock":
+            print("You win!")
+        elif userOption == "scissors" and computerOption == "paper":
+            print("You win!")
+        else:
+            print("The computer wins!")
+
+    # If the player wants to play again or break the loop
+    play_again = input("Do you want to play again? (yes/no): ").lower()
+    if play_again == "no":
+        break
+
+print("Thank you for playing Rock, Paper, Scissors!")
